82ce9860bbe0c41782ddbef9943ff7066a051ef6
Ahmad Mahagna <ahmad.a.10.am@gmail.com>
2024-09-18 10:40:37 +0200

    edited path

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.144.0/build/three.module.js';
-
-//import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'node_modules/three'
+import { OrbitControls } from 'node_modules/three/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import GUI from 'lil-gui'
