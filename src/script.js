4ae0f09f8935be18a4504e6f1e14677a5e897f35
Ahmad Mahagna <ahmad.a.10.am@gmail.com>
2024-09-18 11:18:07 +0200

    jo

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from '/node_modules/three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
